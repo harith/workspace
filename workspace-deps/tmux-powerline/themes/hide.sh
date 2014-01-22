603dbc3f5803054c0e1306e72bde01ee24ec115c
Harith Siddhartha <harith.s@gmail.com>
2014-01-22 22:48:14 +0530

    edited custom tmux powerline theme to look more subtle

--- workspace-deps/tmux-powerline/themes/hide.sh
+++ workspace-deps/tmux-powerline/themes/hide.sh
@@ -23,16 +23,16 @@ TMUX_POWERLINE_DEFAULT_RIGHTSIDE_SEPARATOR=${TMUX_POWERLINE_DEFAULT_RIGHTSIDE_SE
 
 if [ -z $TMUX_POWERLINE_LEFT_STATUS_SEGMENTS ]; then
 	TMUX_POWERLINE_LEFT_STATUS_SEGMENTS=(
-		"tmux_session_info 148 234" \
-		"hostname 33 0" \
+		"tmux_session_info 236 136" \
+		"hostname 235 32" \
 		#"ifstat 30 255" \
 		#"ifstat_sys 30 255" \
 		#"lan_ip 24 255 ${TMUX_POWERLINE_SEPARATOR_RIGHT_THIN}" \
 		#"wan_ip 24 255" \
-		"vcs_branch 29 88" \
-		"vcs_compare 60 255" \
-		"vcs_staged 64 255" \
-		"vcs_modified 9 255" \
+		"vcs_branch 234 250" \
+		"vcs_compare 236 32" \
+		"vcs_staged 235 107" \
+		"vcs_modified 234 124" \
 		"vcs_others 245 0" \
 	)
 fi
@@ -40,13 +40,13 @@ fi
 if [ -z $TMUX_POWERLINE_RIGHT_STATUS_SEGMENTS ]; then
 	TMUX_POWERLINE_RIGHT_STATUS_SEGMENTS=(
 		#"earthquake 3 0" \
-		"pwd 89 211" \
+		"pwd 234 148" \
 		#"mailcount 9 255" \
 		#"now_playing 234 37" \
 		#"cpu 240 136" \
 		#"load 237 167" \
 		#"tmux_mem_cpu_load 234 136" \
-		"battery 234 124" \
+		"battery 233 124" \
 		"weather 235 32" \
 		#"rainbarf 0 0" \
 		#"xkb_layout 125 117" \
